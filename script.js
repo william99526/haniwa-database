77b6e98639ad0a407b667221f0d737eea101c356
william99526 <wisslansinbox@gmail.com>
2026-02-19 15:51:48 +0100

    wi

--- script.js
+++ script.js
@@ -58,6 +58,7 @@ function runSearch() {
       (item.Name && item.Name.toLowerCase().includes(query)) ||
       (item.Kanji && item.Kanji.toLowerCase().includes(query)) ||
       (item.Tomb_name && item.Tomb_name.toLowerCase().includes(query)) ||
+      (item.period-english && item.Period-english.toLowerCase().includes(query))
       (item.Attribute_1 && item.Attribute_1.toLowerCase().includes(query))
     );
   });
